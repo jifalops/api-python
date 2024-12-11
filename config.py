cdc6d80e300b63becfa5556253daaa9be33b9643
Jacob Phillips <jifalops@gmail.com>
2024-12-11 00:20:09 -0500

    feat(devcontainer): setup neo4j

--- config.py
+++ config.py
@@ -3,3 +3,6 @@ import os
 LOGGING_LEVEL: str = os.environ.get("LOGGING_LEVEL", "INFO")
 
 POSTGRES_URI: str = os.environ.get("POSTGRES_URI", "")
+
+NEO4J_URI: str = os.environ.get("NEO4J_URI", "")
+NEO4J_PASSWORD: str = os.environ.get("NEO4J_PASSWORD", "")
